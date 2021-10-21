ab7ec4224f2abfc02a7118d139af04a82e94a4c0
Marlon Andrade <marlon.aswa@gmail.com>
2021-10-20 23:38:22 -0300

    first commit

---
+++ nlw-heat-impulse-react/src/components/LoginBox/index.tsx
@@ -0,0 +1,17 @@
+import { VscGithubInverted } from 'react-icons/vsc'
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth'
+import styles from './styles.module.scss'
+
+export function LoginBox() {
+  const { signInUrl } = useContext(AuthContext);
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signWithGithubButton}>
+        <VscGithubInverted size={24} />
+        Entrar com Github
+      </a>
+    </div>
+  )
+}
